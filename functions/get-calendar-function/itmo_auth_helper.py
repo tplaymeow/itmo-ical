a8311b21148c95639a827c00efd50cc63291d3bf
Timur Guliamov <wersa2002@gmail.com>
2025-10-18 20:20:17 +0300

    Fixed auth flow

--- functions/get-calendar-function/itmo_auth_helper.py
+++ functions/get-calendar-function/itmo_auth_helper.py
@@ -4,29 +4,32 @@ import re
 import urllib.parse
 from base64 import urlsafe_b64encode
 from hashlib import sha256
+import logging
 
 import requests
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 _CLIENT_ID = "student-personal-cabinet"
 _REDIRECT_URI = "https://my.itmo.ru/login/callback"
 _PROVIDER = "https://id.itmo.ru/auth/realms/itmo"
-_FORM_ACTION_REGEX = re.compile(r'<form\s+.*?\s+action="(?P<action>.*?)"', re.DOTALL)
 
 
-def generate_code_verifier():
-    code_verifier = urlsafe_b64encode(os.urandom(40)).decode("utf-8")
-    return re.sub("[^a-zA-Z0-9]+", "", code_verifier)
+def generate_code_verifier() -> str:
+    return urlsafe_b64encode(os.urandom(40)).rstrip(b"=").decode("ascii")
 
 
-def get_code_challenge(code_verifier: str):
-    code_challenge_bytes = sha256(code_verifier.encode("utf-8")).digest()
-    code_challenge = urlsafe_b64encode(code_challenge_bytes).decode("utf-8")
-    return code_challenge.replace("=", "")  # remove base64 padding
+def get_code_challenge(code_verifier: str) -> str:
+    digest = sha256(code_verifier.encode("ascii")).digest()
+    return urlsafe_b64encode(digest).rstrip(b"=").decode("ascii")
 
 
 def extract_form_action(text: str) -> str:
-    form_action_match = _FORM_ACTION_REGEX.search(text)
-    return html.unescape(form_action_match.group("action"))
+    session_code = re.search(r"session_code=([^&\"']+)", text).group(1)
+    execution = re.search(r"execution=([^&\"']+)", text).group(1)
+    tab_id = re.search(r"tab_id=([^&\"']+)", text).group(1)
+    return f"{_PROVIDER}/login-actions/authenticate?session_code={session_code}&execution={execution}&tab_id={tab_id}&client_id={_CLIENT_ID}"
 
 
 def extract_auth_code(redirect_url: str) -> str:
@@ -36,9 +39,11 @@ def extract_auth_code(redirect_url: str) -> str:
 
 
 def get_access_token(login: str, password: str) -> str:
+    logger.info("Generating code_verifier and code_challenge")
     code_verifier = generate_code_verifier()
     code_challenge = get_code_challenge(code_verifier)
 
+    logger.info("Fetching auth page")
     auth_resp = requests.get(
         _PROVIDER + "/protocol/openid-connect/auth",
         params={
@@ -54,8 +59,11 @@ def get_access_token(login: str, password: str) -> str:
     )
     auth_resp.raise_for_status()
 
+    logger.info("Extraction form action")
     form_action = extract_form_action(auth_resp.text)
+    logger.info(f"Extracted action url: {form_action}")
 
+    logger.info("Requesting auth information")
     form_resp = requests.post(
         url=form_action,
         data={"username": login, "password": password},
@@ -65,8 +73,10 @@ def get_access_token(login: str, password: str) -> str:
     if form_resp.status_code != 302:
         raise ValueError(f"Wrong Keycloak form response: {form_resp.status_code} {form_resp.text}")
 
+    logger.info("Extracting auth code")
     auth_code = extract_auth_code(form_resp.headers["Location"])
 
+    logger.info("Fetching auth tokens")
     token_resp = requests.post(
         url=_PROVIDER + "/protocol/openid-connect/token",
         data={
--- terraform/production/.terraform.lock.hcl
+++ terraform/production/.terraform.lock.hcl
@@ -2,15 +2,15 @@
 # Manual edits may be lost in future updates.
 
 provider "registry.terraform.io/hashicorp/archive" {
-  version = "2.6.0"
+  version = "2.7.1"
   hashes = [
-    "h1:s1OObC0b95ceQkrAMqL4q6wMYDBWYt8swZbLup+UJXI=",
+    "h1:A7EnRBVm4h9ryO9LwxYnKr4fy7ExPMwD5a1DsY7m1Y0=",
   ]
 }
 
 provider "registry.terraform.io/yandex-cloud/yandex" {
-  version = "0.131.0"
+  version = "0.165.0"
   hashes = [
-    "h1:rcIsM5BSwObdPcpkJE5OfRvZX4CFOdqQ7Bzkg4H1SX8=",
+    "h1:9q6d0otqIYHofrPQ49zvvNBAnOGLAE5jRXm9fpX0b1w=",
   ]
 }
